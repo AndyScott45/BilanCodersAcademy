f76a02201a6d07bc36aec6f72691f31cdeb83533
Andy Biira <biiraledum@gmail.com>
2021-12-12 17:39:07 -0800

    fifth commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.menu laterali')
 
 @section('content')
 <div class="container">
-<div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12 mx-auto p-0">
-    <div class="card">
-    <div class="login-box">
-    <div class="login-snip card-header">{{ __('Dashboard') }}</div>
-    <div class="login-space">
-    <div class="login card-body">
-                          @if (session('status'))
-                             <div class="alert alert-success" role="alert">
-                                 {{ session('status') }}
-                                   </div>
-                                      @endif
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
 
-                           You are logged in!
-                          </div>
-                       </div>
-                    </div>
-                 </div>
-              </div>
-           </div>
-      </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 @endsection
